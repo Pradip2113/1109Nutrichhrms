907b70a33d7b36c3dc566704b09e74d169c7750e
pradip2113 <21pradipjadhav@gmail.com>
2023-09-11 13:58:59 +0530

    first commit

---
+++ hrms/hr/doctype/employee_checkin/employee_checkin.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Checkin', {
+	// log_type: function(frm) {
+	// 	frm.call({
+	// 		method:'calot',
+	// 		doc: frm.doc, 
+	// 	});
+	// }
+});
